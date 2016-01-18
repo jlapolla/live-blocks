18c09ad1ba34b7dde673af0225a656fe9444ad9a
Justin LaPolla <j.a.lapolla@gmail.com>
2016-01-18 05:18:04 +0000

    Use Karma code coverage
    - Serve files from test/ on 8080 to access code coverage reports.
    - Hint: Use http-server from NPM to serve test/.

--- karma.conf.js
+++ karma.conf.js
@@ -28,13 +28,18 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'live-blocks.js': 'coverage'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      type: 'html',
+      dir: 'test'
+    },
 
 
     // web server port
