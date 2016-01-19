bd2a368fcb0e747bd6f5fef2d1e5b44536ead9e1
Justin LaPolla <j.a.lapolla@gmail.com>
2016-01-19 02:55:42 +0000

    Add Block class tests
    - Just committing my progress as I go.

--- spec/Block.js
+++ spec/Block.js
@@ -117,6 +117,62 @@ describe("Block class", function(){
     expect(runLog[7]).toBe(blocks[1]);
   });
 
+  it("automatically overwrites sourced outputs with a value", function(){
+
+    // Create a static output block
+    var block = new LiveBlocks.Block();
+    block.run = function(){
+      this.prop("output", true);
+    };
+
+    // Try overwriting the output
+    block.prop("output", false);
+    expect(block.prop("output")).toBe(true);
+
+    // Try overwriting the output
+    block.prop("output", 3);
+    expect(block.prop("output")).toBe(true);
+  });
+
+  it("detaches from old sources", function(){
+
+    // Create some inverter blocks which log their calls
+    var runLog = [];
+    var blocks = [];
+    var run = function(){
+      runLog.push(this);
+      this.prop("output", !this.prop("input"));
+    };
+    for (var i = 0; i < 3; i++){
+      var block = new LiveBlocks.Block();
+      block.run = run;
+      blocks.push(block);
+    }
+
+    // Connect blocks in first configuration
+    blocks[2].prop("input", blocks[0], "output");
+
+    // Test stimulus
+    blocks[0].prop("input", true);
+    expect(runLog.length).toBe(4);
+    expect(runLog[0]).toBe(blocks[0]);
+    expect(runLog[1]).toBe(blocks[0]);
+    expect(runLog[2]).toBe(blocks[2]);
+    expect(runLog[3]).toBe(blocks[2]);
+
+    // Connect blocks in second configuration
+    blocks[2].prop("input", blocks[1], "output");
+
+    // Reset runLog
+    runLog.length = 0;
+
+    // Test stimulus
+    blocks[0].prop("input", false);
+    expect(runLog.length).toBe(2);
+    expect(runLog[0]).toBe(blocks[0]);
+    expect(runLog[1]).toBe(blocks[0]);
+  });
+
   it("can delete properties", function(){});
 
   it("does not hang when .run() is undefined", function(){});
