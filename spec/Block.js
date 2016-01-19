7178f2c3370a540c28778631ba8d8b4daca1e31c
Justin LaPolla <j.a.lapolla@gmail.com>
2016-01-19 02:30:52 +0000

    Add Block class tests
    - Just committing my progress as I go.

--- spec/Block.js
+++ spec/Block.js
@@ -40,6 +40,83 @@ describe("Block class", function(){
     expect(blocks[2].prop("output")).toEqual(true);
   });
 
+  it("updates linked objects efficiently", function(){
+
+    // Create some inverter blocks which log their calls
+    var runLog = [];
+    var blocks = [];
+    var run = function(){
+      runLog.push(this);
+      this.prop("output", !this.prop("input"));
+    };
+    for (var i = 0; i < 3; i++){
+      var block = new LiveBlocks.Block();
+      block.run = run;
+      blocks.push(block);
+    }
+
+    // Connect inverter blocks
+    blocks[1].prop("input", blocks[0], "output");
+    blocks[2].prop("input", blocks[1], "output");
+
+    // Test stimulus
+    blocks[0].prop("input", true);
+    expect(runLog.length).toBe(6);
+    expect(runLog[0]).toBe(blocks[0]);
+    expect(runLog[1]).toBe(blocks[0]);
+    expect(runLog[2]).toBe(blocks[1]);
+    expect(runLog[3]).toBe(blocks[1]);
+    expect(runLog[4]).toBe(blocks[2]);
+    expect(runLog[5]).toBe(blocks[2]);
+
+    // Clear runLog
+    runLog.length = 0;
+
+    // Switch test stimulus
+    blocks[0].prop("input", false);
+    expect(runLog.length).toBe(6);
+    expect(runLog[0]).toBe(blocks[0]);
+    expect(runLog[1]).toBe(blocks[0]);
+    expect(runLog[2]).toBe(blocks[1]);
+    expect(runLog[3]).toBe(blocks[1]);
+    expect(runLog[4]).toBe(blocks[2]);
+    expect(runLog[5]).toBe(blocks[2]);
+  });
+
+  it("handles block feedback efficiently", function(){
+
+    // Create some inverter blocks which log their calls
+    var runLog = [];
+    var blocks = [];
+    var run = function(){
+      runLog.push(this);
+      this.prop("A", this.prop("a"));
+      this.prop("B", this.prop("b"));
+    };
+    for (var i = 0; i < 3; i++){
+      var block = new LiveBlocks.Block();
+      block.run = run;
+      blocks.push(block);
+    }
+
+    // Connect blocks
+    blocks[1].prop("a", blocks[0], "A");
+    blocks[0].prop("b", blocks[1], "A");
+    blocks[1].prop("b", blocks[0], "B");
+
+    // Test stimulus
+    blocks[0].prop("a", true);
+    expect(runLog.length).toBe(8);
+    expect(runLog[0]).toBe(blocks[0]);
+    expect(runLog[1]).toBe(blocks[0]);
+    expect(runLog[2]).toBe(blocks[1]);
+    expect(runLog[3]).toBe(blocks[1]);
+    expect(runLog[4]).toBe(blocks[0]);
+    expect(runLog[5]).toBe(blocks[0]);
+    expect(runLog[6]).toBe(blocks[1]);
+    expect(runLog[7]).toBe(blocks[1]);
+  });
+
   it("can delete properties", function(){});
 
   it("does not hang when .run() is undefined", function(){});
