f82d295c8e92b41e492d5cc11f35c8ed0349d98a
Justin LaPolla <j.a.lapolla@gmail.com>
2016-01-21 04:48:16 +0000

    Add multiInheritClass tests
    - Achieved 100% code coverage in multiInheritClass function.

--- classes/multiInheritClass.js
+++ classes/multiInheritClass.js
@@ -2,6 +2,7 @@ this.multiInheritClass = function(base, derived){
 
   // Fake multiple inheritance
   for (var name in base.prototype) {
+
     if (name !== "constructor")
       derived.prototype[name] = base.prototype[name];
   }
--- spec/multiInheritClass.js
+++ spec/multiInheritClass.js
@@ -49,6 +49,61 @@ describe("multiInheritClass function", function(){
     expect(object.talk()).toBe("I talk the talk");
   });
 
-  xit("does not overwrite the constructor", function(){});
+  it("inherits from the base class and all ancestors of the base class", function(){
+
+    // Make Mover class
+    function Mover(){
+      this._myMove = "I'm a mover";
+    }
+    Mover.prototype = {};
+    Mover.prototype.move = function(){
+      return this._myMove;
+    };
+
+    // Make Walker class
+    function Walker(){
+      Mover.call(this);
+      this._myWalk = "I walk the walk";
+    }
+    LiveBlocks.extendClass(Mover, Walker);
+    Walker.prototype.walk = function(){
+      return this._myWalk;
+    };
+
+    // Make Talker class
+    function Talker(){
+      this._myTalk = "I talk the talk";
+    }
+    Talker.prototype = {};
+    Talker.prototype.talk = function(){
+      return this._myTalk;
+    };
+
+    // Make WalkerAndTalker class
+    function WalkerAndTalker(){
+      Talker.call(this);
+      Walker.call(this);
+    }
+    LiveBlocks.extendClass(Talker, WalkerAndTalker);
+    LiveBlocks.multiInheritClass(Walker, WalkerAndTalker);
+    WalkerAndTalker.prototype.walkAndTalk = function(){
+      return this.walk() + " and " + this.talk();
+    };
+
+    // Instantiate WalkerAndTalker
+    var object = new WalkerAndTalker();
+
+    // Check inheritance with instanceof
+    expect(object instanceof WalkerAndTalker).toBe(true);
+    expect(object instanceof Talker).toBe(true);
+    expect(object instanceof Walker).toBe(false); // Javascript doesn't support multiple inheritance
+    expect(object instanceof Mover).toBe(false); // Javascript doesn't support multiple inheritance
+
+    // Check functions
+    expect(object.walkAndTalk()).toBe("I walk the walk and I talk the talk");
+    expect(object.talk()).toBe("I talk the talk");
+    expect(object.walk()).toBe("I walk the walk");
+    expect(object.move()).toBe("I'm a mover");
+  });
 });
 
