500eea3a6bad15c3febae5f3351fbd99964c30e7
Justin LaPolla <j.a.lapolla@gmail.com>
2016-01-23 04:34:00 +0000

    Apply Subject spec to Subject and Block classes
    - Block inherits from Subject. Have to test it's Subject functions too.

--- spec/Subject.js
+++ spec/Subject.js
@@ -1,242 +1,251 @@
 "use strict";
 
-describe("Subject class", function(){
+(function(classNames){
+  
+  for (var i = 0; i < classNames.length; i++){
 
-  var LiveBlocks = window.LiveBlocks;
+    (function(name){
 
-  it("does not attach duplicate observers", function(){
+      describe(name + " class", function(){
 
-    // Create a Subject
-    var subject = new LiveBlocks.Subject();
-    expect(subject._observers).toEqual({});
-    
-    // Attach the first observer
-    var observers = [{}, {}, {}];
-    subject.attach(observers[0], "x");
-    expect(subject._observers).toEqual({"x": [{}]});
-    expect(subject._observers["x"][0]).toBe(observers[0]);
+        var LiveBlocks = window.LiveBlocks;
 
-    // Attach a new observer
-    subject.attach(observers[1], "x");
-    expect(subject._observers).toEqual({"x": [{}, {}]});
-    expect(subject._observers["x"][0]).toBe(observers[0]);
-    expect(subject._observers["x"][1]).toBe(observers[1]);
+        it("does not attach duplicate observers", function(){
 
-    // Attach the same observer again
-    subject.attach(observers[0], "x");
-    expect(subject._observers).toEqual({"x": [{}, {}]});
-    expect(subject._observers["x"][0]).toBe(observers[0]);
-    expect(subject._observers["x"][1]).toBe(observers[1]);
+          // Create a Subject
+          var subject = new LiveBlocks[name]();
+          expect(subject._observers).toEqual({});
+          
+          // Attach the first observer
+          var observers = [{}, {}, {}];
+          subject.attach(observers[0], "x");
+          expect(subject._observers).toEqual({"x": [{}]});
+          expect(subject._observers["x"][0]).toBe(observers[0]);
 
-    // Attach two different observers to "y"
-    subject.attach(observers[0], "y");
-    subject.attach(observers[2], "y");
-    expect(subject._observers).toEqual({"x": [{}, {}], "y": [{}, {}]});
-    expect(subject._observers["x"][0]).toBe(observers[0]);
-    expect(subject._observers["x"][1]).toBe(observers[1]);
-    expect(subject._observers["y"][0]).toBe(observers[0]);
-    expect(subject._observers["y"][1]).toBe(observers[2]);
-  });
+          // Attach a new observer
+          subject.attach(observers[1], "x");
+          expect(subject._observers).toEqual({"x": [{}, {}]});
+          expect(subject._observers["x"][0]).toBe(observers[0]);
+          expect(subject._observers["x"][1]).toBe(observers[1]);
 
-  it("does not delete an observer list if any observers remain", function(){
+          // Attach the same observer again
+          subject.attach(observers[0], "x");
+          expect(subject._observers).toEqual({"x": [{}, {}]});
+          expect(subject._observers["x"][0]).toBe(observers[0]);
+          expect(subject._observers["x"][1]).toBe(observers[1]);
 
-    // Create a subject
-    var subject = new LiveBlocks.Subject();
-    expect(subject._observers).toEqual({});
+          // Attach two different observers to "y"
+          subject.attach(observers[0], "y");
+          subject.attach(observers[2], "y");
+          expect(subject._observers).toEqual({"x": [{}, {}], "y": [{}, {}]});
+          expect(subject._observers["x"][0]).toBe(observers[0]);
+          expect(subject._observers["x"][1]).toBe(observers[1]);
+          expect(subject._observers["y"][0]).toBe(observers[0]);
+          expect(subject._observers["y"][1]).toBe(observers[2]);
+        });
 
-    // Attach some observers
-    var observers = [{}, {}, {}];
-    subject.attach(observers[0], "x");
-    subject.attach(observers[1], "x");
-    subject.attach(observers[0], "y");
-    subject.attach(observers[2], "y");
-    expect(subject._observers).toEqual({"x": [{}, {}], "y": [{}, {}]});
+        it("does not delete an observer list if any observers remain", function(){
 
-    // Detach first "x" observer
-    subject.detach(observers[0], "x");
-    expect(subject._observers).toEqual({"x": [{}], "y": [{}, {}]});
-    expect(subject._observers["x"][0]).toBe(observers[1]);
-  });
+          // Create a subject
+          var subject = new LiveBlocks[name]();
+          expect(subject._observers).toEqual({});
 
-  it("deletes an observer list when the last observer is detached", function(){
+          // Attach some observers
+          var observers = [{}, {}, {}];
+          subject.attach(observers[0], "x");
+          subject.attach(observers[1], "x");
+          subject.attach(observers[0], "y");
+          subject.attach(observers[2], "y");
+          expect(subject._observers).toEqual({"x": [{}, {}], "y": [{}, {}]});
 
-    // Create a subject
-    var subject = new LiveBlocks.Subject();
-    expect(subject._observers).toEqual({});
+          // Detach first "x" observer
+          subject.detach(observers[0], "x");
+          expect(subject._observers).toEqual({"x": [{}], "y": [{}, {}]});
+          expect(subject._observers["x"][0]).toBe(observers[1]);
+        });
 
-    // Attach some observers
-    var observers = [{}, {}, {}];
-    subject.attach(observers[0], "x");
-    subject.attach(observers[1], "x");
-    subject.attach(observers[0], "y");
-    subject.attach(observers[2], "y");
-    expect(subject._observers).toEqual({"x": [{}, {}], "y": [{}, {}]});
+        it("deletes an observer list when the last observer is detached", function(){
 
-    // Detach all "x" observers
-    subject.detach(observers[0], "x");
-    subject.detach(observers[1], "x");
-    expect(subject._observers).toEqual({"y": [{}, {}]});
-  });
+          // Create a subject
+          var subject = new LiveBlocks[name]();
+          expect(subject._observers).toEqual({});
 
-  it("does nothing when a non-existent observers is detached", function(){
+          // Attach some observers
+          var observers = [{}, {}, {}];
+          subject.attach(observers[0], "x");
+          subject.attach(observers[1], "x");
+          subject.attach(observers[0], "y");
+          subject.attach(observers[2], "y");
+          expect(subject._observers).toEqual({"x": [{}, {}], "y": [{}, {}]});
 
-    // Create a subject
-    var subject = new LiveBlocks.Subject();
-    expect(subject._observers).toEqual({});
+          // Detach all "x" observers
+          subject.detach(observers[0], "x");
+          subject.detach(observers[1], "x");
+          expect(subject._observers).toEqual({"y": [{}, {}]});
+        });
 
-    // Attach some observers
-    var observers = [{}, {}, {}];
-    subject.attach(observers[0], "x");
-    subject.attach(observers[1], "x");
-    subject.attach(observers[0], "y");
-    subject.attach(observers[2], "y");
-    expect(subject._observers).toEqual({"x": [{}, {}], "y": [{}, {}]});
-    expect(subject._observers["x"][0]).toBe(observers[0]);
-    expect(subject._observers["x"][1]).toBe(observers[1]);
+        it("does nothing when a non-existent observers is detached", function(){
 
-    // Detach non-existent observer
-    subject.detach(observers[2], "x");
-    expect(subject._observers).toEqual({"x": [{}, {}], "y": [{}, {}]});
-    expect(subject._observers["x"][0]).toBe(observers[0]);
-    expect(subject._observers["x"][1]).toBe(observers[1]);
-  });
+          // Create a subject
+          var subject = new LiveBlocks[name]();
+          expect(subject._observers).toEqual({});
 
-  it("does nothing when an observer is detached from a non-observed property", function(){
+          // Attach some observers
+          var observers = [{}, {}, {}];
+          subject.attach(observers[0], "x");
+          subject.attach(observers[1], "x");
+          subject.attach(observers[0], "y");
+          subject.attach(observers[2], "y");
+          expect(subject._observers).toEqual({"x": [{}, {}], "y": [{}, {}]});
+          expect(subject._observers["x"][0]).toBe(observers[0]);
+          expect(subject._observers["x"][1]).toBe(observers[1]);
 
-    // Create a subject
-    var subject = new LiveBlocks.Subject();
-    expect(subject._observers).toEqual({});
+          // Detach non-existent observer
+          subject.detach(observers[2], "x");
+          expect(subject._observers).toEqual({"x": [{}, {}], "y": [{}, {}]});
+          expect(subject._observers["x"][0]).toBe(observers[0]);
+          expect(subject._observers["x"][1]).toBe(observers[1]);
+        });
 
-    // Attach some observers
-    var observers = [{}, {}, {}];
-    subject.attach(observers[0], "x");
-    subject.attach(observers[1], "x");
-    expect(subject._observers).toEqual({"x": [{}, {}]});
-    expect(subject._observers["x"][0]).toBe(observers[0]);
-    expect(subject._observers["x"][1]).toBe(observers[1]);
+        it("does nothing when an observer is detached from a non-observed property", function(){
 
-    // Detach from non-observed property
-    subject.detach(observers[0], "y");
-    expect(subject._observers).toEqual({"x": [{}, {}]});
-    expect(subject._observers["x"][0]).toBe(observers[0]);
-    expect(subject._observers["x"][1]).toBe(observers[1]);
-  });
+          // Create a subject
+          var subject = new LiveBlocks[name]();
+          expect(subject._observers).toEqual({});
 
-  it(".notify() function calls .update() once on each observer", function(){
+          // Attach some observers
+          var observers = [{}, {}, {}];
+          subject.attach(observers[0], "x");
+          subject.attach(observers[1], "x");
+          expect(subject._observers).toEqual({"x": [{}, {}]});
+          expect(subject._observers["x"][0]).toBe(observers[0]);
+          expect(subject._observers["x"][1]).toBe(observers[1]);
 
-    // Create a subject
-    var subject = new LiveBlocks.Subject();
+          // Detach from non-observed property
+          subject.detach(observers[0], "y");
+          expect(subject._observers).toEqual({"x": [{}, {}]});
+          expect(subject._observers["x"][0]).toBe(observers[0]);
+          expect(subject._observers["x"][1]).toBe(observers[1]);
+        });
 
-    // Set up observers
-    var updateRecord = [];
-    var updateFn = function(){
-      updateRecord.push(this);
-    };
-    var observers = [{}, {}, {}];
-    for (var i = 0; i < observers.length; i++)
-      observers[i].update = updateFn;
+        it(".notify() function calls .update() once on each observer", function(){
 
-    // Attach observers
-    subject.attach(observers[0], "x");
-    subject.attach(observers[1], "x");
-    subject.attach(observers[0], "y");
-    subject.attach(observers[2], "y");
+          // Create a subject
+          var subject = new LiveBlocks[name]();
 
-    // Run .notify("x")
-    subject.notify("x");
-    expect(updateRecord.length).toBe(2);
-    expect(updateRecord[0]).toBe(observers[0]);
-    expect(updateRecord[1]).toBe(observers[1]);
+          // Set up observers
+          var updateRecord = [];
+          var updateFn = function(){
+            updateRecord.push(this);
+          };
+          var observers = [{}, {}, {}];
+          for (var i = 0; i < observers.length; i++)
+            observers[i].update = updateFn;
 
-    // Clear updateRecord
-    updateRecord.length = 0;
+          // Attach observers
+          subject.attach(observers[0], "x");
+          subject.attach(observers[1], "x");
+          subject.attach(observers[0], "y");
+          subject.attach(observers[2], "y");
 
-    // Run .notify("y")
-    subject.notify("y");
-    expect(updateRecord.length).toBe(2);
-    expect(updateRecord[0]).toBe(observers[0]);
-    expect(updateRecord[1]).toBe(observers[2]);
-  });
+          // Run .notify("x")
+          subject.notify("x");
+          expect(updateRecord.length).toBe(2);
+          expect(updateRecord[0]).toBe(observers[0]);
+          expect(updateRecord[1]).toBe(observers[1]);
 
-  it(".notify() does nothing when called on a non-observed property", function(){
+          // Clear updateRecord
+          updateRecord.length = 0;
 
-    // Create a subject
-    var subject = new LiveBlocks.Subject();
+          // Run .notify("y")
+          subject.notify("y");
+          expect(updateRecord.length).toBe(2);
+          expect(updateRecord[0]).toBe(observers[0]);
+          expect(updateRecord[1]).toBe(observers[2]);
+        });
 
-    // Set up observers
-    var updateRecord = [];
-    var updateFn = function(){
-      updateRecord.push(this);
-    };
-    var observers = [{}, {}, {}];
-    for (var i = 0; i < observers.length; i++)
-      observers[i].update = updateFn;
+        it(".notify() does nothing when called on a non-observed property", function(){
 
-    // Attach observers
-    subject.attach(observers[0], "x");
-    subject.attach(observers[1], "x");
-    subject.attach(observers[0], "y");
-    subject.attach(observers[2], "y");
+          // Create a subject
+          var subject = new LiveBlocks[name]();
 
-    // Run .notify("z")
-    subject.notify("z");
-    expect(updateRecord.length).toBe(0);
-  });
+          // Set up observers
+          var updateRecord = [];
+          var updateFn = function(){
+            updateRecord.push(this);
+          };
+          var observers = [{}, {}, {}];
+          for (var i = 0; i < observers.length; i++)
+            observers[i].update = updateFn;
 
-  it(".notify() calls update once on each observer, even if observers are detached during .notify()", function(){
+          // Attach observers
+          subject.attach(observers[0], "x");
+          subject.attach(observers[1], "x");
+          subject.attach(observers[0], "y");
+          subject.attach(observers[2], "y");
 
-    // Create a subject
-    var subject = new LiveBlocks.Subject();
+          // Run .notify("z")
+          subject.notify("z");
+          expect(updateRecord.length).toBe(0);
+        });
 
-    // Set up observers
-    var observers = [{}, {}, {}];
-    var updateRecord = [];
-    var updateFn = function(){
-      updateRecord.push(this);
-      subject.detach(observers[1], "x");
-    };
-    for (var i = 0; i < observers.length; i++)
-      observers[i].update = updateFn;
+        it(".notify() calls update once on each observer, even if observers are detached during .notify()", function(){
 
-    // Attach observers
-    subject.attach(observers[0], "x");
-    subject.attach(observers[1], "x");
-    subject.attach(observers[2], "x");
+          // Create a subject
+          var subject = new LiveBlocks[name]();
 
-    // Run .notify("x")
-    subject.notify("x");
-    expect(updateRecord.length).toBe(3);
-    expect(updateRecord[0]).toBe(observers[0]);
-    expect(updateRecord[1]).toBe(observers[1]);
-    expect(updateRecord[2]).toBe(observers[2]);
-    expect(subject._observers["x"].length).toBe(2);
-  });
+          // Set up observers
+          var observers = [{}, {}, {}];
+          var updateRecord = [];
+          var updateFn = function(){
+            updateRecord.push(this);
+            subject.detach(observers[1], "x");
+          };
+          for (var i = 0; i < observers.length; i++)
+            observers[i].update = updateFn;
 
-  it(".notify() ignores observers attached during .notify()", function(){
+          // Attach observers
+          subject.attach(observers[0], "x");
+          subject.attach(observers[1], "x");
+          subject.attach(observers[2], "x");
 
-    // Create a subject
-    var subject = new LiveBlocks.Subject();
+          // Run .notify("x")
+          subject.notify("x");
+          expect(updateRecord.length).toBe(3);
+          expect(updateRecord[0]).toBe(observers[0]);
+          expect(updateRecord[1]).toBe(observers[1]);
+          expect(updateRecord[2]).toBe(observers[2]);
+          expect(subject._observers["x"].length).toBe(2);
+        });
 
-    // Set up observers
-    var observers = [{}, {}, {}];
-    var updateRecord = [];
-    var updateFn = function(){
-      updateRecord.push(this);
-      subject.attach(observers[2], "x");
-    };
-    for (var i = 0; i < observers.length; i++)
-      observers[i].update = updateFn;
+        it(".notify() ignores observers attached during .notify()", function(){
 
-    // Attach observers
-    subject.attach(observers[0], "x");
-    subject.attach(observers[1], "x");
+          // Create a subject
+          var subject = new LiveBlocks[name]();
 
-    // Run .notify("x")
-    subject.notify("x");
-    expect(updateRecord.length).toBe(2);
-    expect(updateRecord[0]).toBe(observers[0]);
-    expect(updateRecord[1]).toBe(observers[1]);
-    expect(subject._observers["x"].length).toBe(3);
-  });
-});
+          // Set up observers
+          var observers = [{}, {}, {}];
+          var updateRecord = [];
+          var updateFn = function(){
+            updateRecord.push(this);
+            subject.attach(observers[2], "x");
+          };
+          for (var i = 0; i < observers.length; i++)
+            observers[i].update = updateFn;
+
+          // Attach observers
+          subject.attach(observers[0], "x");
+          subject.attach(observers[1], "x");
+
+          // Run .notify("x")
+          subject.notify("x");
+          expect(updateRecord.length).toBe(2);
+          expect(updateRecord[0]).toBe(observers[0]);
+          expect(updateRecord[1]).toBe(observers[1]);
+          expect(subject._observers["x"].length).toBe(3);
+        });
+      });
+    }(classNames[i]));
+  }
+}(["Subject", "Block"]));
 
