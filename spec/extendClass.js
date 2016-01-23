9b55a1d7250d0caa6eea03beb50c6e34b7d1e46c
Justin LaPolla <j.a.lapolla@gmail.com>
2016-01-23 04:06:33 +0000

    Add extendClass spec
    - Just being thorough.

---
+++ spec/extendClass.js
@@ -0,0 +1,40 @@
+"use strict";
+
+describe("extendClass function", function(){
+
+  var LiveBlocks = window.LiveBlocks;
+
+  it("extends a class", function(){
+
+    // Make Mover class
+    function Mover(){
+      this._myMove = "I'm a mover";
+    }
+    Mover.prototype = {};
+    Mover.prototype.move = function(){
+      return this._myMove;
+    };
+
+    // Make Walker class
+    function Walker(){
+      Mover.call(this);
+      this._myWalk = "I walk the walk";
+    }
+    LiveBlocks.extendClass(Mover, Walker);
+    Walker.prototype.walk = function(){
+      return this._myWalk;
+    };
+
+    // Instatiate Walker
+    var object = new Walker();
+
+    // Check inheritance with instanceof
+    expect(object instanceof Walker).toBe(true);
+    expect(object instanceof Mover).toBe(true);
+
+    // Check functions
+    expect(object.walk()).toBe("I walk the walk");
+    expect(object.move()).toBe("I'm a mover");
+  });
+});
+
