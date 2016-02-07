0374c3820a593db60d2dac7a5c17575329f6ffc7
Justin LaPolla <j.a.lapolla@gmail.com>
2016-02-07 16:14:54 +0000

    BlackBox init() and duplicate()
    - Prototype BlackBox init() and duplicate() functions. These prototypes
      are untested.

--- classes/BlackBox.js
+++ classes/BlackBox.js
@@ -1,17 +1,69 @@
-this.BlackBox = (function(EventEmitter, extendClass, hasOwnProperty, Queue, getUndefined){
+this.BlackBox = (function(EventEmitter, extendClass, hasOwnProperty, Queue, getUndefined, Map, Set){
   var errorListener = function(e){
 
     if (!hasOwnProperty(this, "_lastError"))
       this._lastError = e;
   };
-  var init = function(pins){
+  var init = (function(){
 
-    var wires = [], blocks = [];
-    for (var name in pins){
+    var processWire, processBlock;
 
-      // TODO Finish this function
-    }
-  };
+    processWire = function(wire, wireSet, blockSet){
+
+      // Check if this wire has already been processed
+      if (!wireSet.has(wire)){
+
+        // Wire has not been processed
+        // Add wire to wire set
+        wireSet.add(wire);
+
+        // Process all blocks connected to the wire
+        var it = wire.connections();
+        while (!it.peek().done)
+          processBlock(it.next().value.block, wireSet, blockSet);
+      }
+    };
+
+    processBlock = function(block, wireSet, blockSet){
+
+      // Check if this block has already been processed
+      if (!blockSet.has(block)){
+
+        // Block has not been processed
+        // Add block to block set
+        blockSet.add(block);
+
+        // Process all wires connected to the block
+        var it = block.connections();
+        while (!it.peek().done)
+          processWire(it.next().value.wire, wireSet, blockSet);
+      }
+    };
+
+    return function(pins){
+
+      // Create wire set and block set
+      // These collect all wires and blocks in the network
+      var wireSet = new Set();
+      var blockSet = new Set();
+
+      // Process pins hash
+      for (var pin in pins){
+
+        // Add pin to internal wire hash
+        this._internalWires[pin] = pins[pin].wire;
+
+        // Process the wire and all connected blocks
+        processWire(pins[pin].wire, wireSet, blockSet);
+      }
+
+      // All blocks are in the block set now
+      // Attach "error" event listener to all blocks in the network
+      var it = blockSet.values();
+      while (!it.peek().done)
+        it.next().value.on("error", this._errorListener);
+    };
+  }());
   function BlackBox(hash){
 
     EventEmitter.call(this);
@@ -38,6 +90,78 @@ this.BlackBox = (function(EventEmitter, extendClass, hasOwnProperty, Queue, getU
   }
   extendClass(EventEmitter, BlackBox);
   var P = BlackBox.prototype;
+  P.duplicate = (function(){
+
+    var processWire, processBlock;
+
+    processWire = function(wire, wireMap, blockMap){
+
+      // Check if this wire has already been processed
+      var duplicate = wireMap.get(wire);
+      if (duplicate)
+        return duplicate;
+
+      // Wire has not been processed
+      // Duplicate the wire
+      duplicate = wire.duplicate();
+
+      // Add duplicate wire to wire map
+      wireMap.put(wire, duplicate);
+
+      // Process all blocks connected to the wire
+      var it = wire.connections();
+      while (!it.peek().done)
+        processBlock(it.next().value.block, wireMap, blockMap);
+
+      // Return duplicate wire
+      return duplicate;
+    };
+
+    processBlock = function(block, wireMap, blockMap){
+
+      // Check if this block has already been processed
+      var duplicate = blockMap.get(block);
+      if (duplicate)
+        return duplicate;
+
+      // Block has not been processed
+      // Duplicate the block
+      duplicate = block.duplicate();
+
+      // Add duplicate block to block map
+      blockMap.put(block, duplicate);
+
+      // Process all wires connected to the block
+      var it = block.connections();
+      while (!it.peek().done){
+
+        // Process and get the duplicate wire
+        var wire = processWire(it.peek().value.wire, wireMap, blockMap);
+
+        // Connect the duplicate block to the duplicate wire
+        duplicate.connect(it.next().value.pin, wire);
+      }
+
+      // Return duplicate block
+      return duplicate;
+    };
+
+    return function(){
+
+      // Create wire map and block map
+      // These map wires and blocks in the original circuit to their duplicates
+      var wireMap = new Map();
+      var blockMap = new Map();
+
+      // Construct pins hash for the new BlackBox
+      var pins;
+      for (var pin in this._internalWires)
+        pins[pin] = processWire(this._internalWires[pin], wireMap, blockMap);
+
+      // Return the new BlackBox
+      return new BlackBox({pins: pins, queue: this._updateQueue.duplicate()});
+    };
+  }());
   P.update = function(pin){
 
     // A connected wire value changed
@@ -116,5 +240,5 @@ this.BlackBox = (function(EventEmitter, extendClass, hasOwnProperty, Queue, getU
     }
   };
   return BlackBox;
-}(this.EventEmitter, this.extendClass, this.hasOwnProperty, this.Queue, this.getUndefined));
+}(this.EventEmitter, this.extendClass, this.hasOwnProperty, this.Queue, this.getUndefined, this.Map, this.Set));
 
