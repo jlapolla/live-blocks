604cc19d8c1a80f6aa21b98357e3aafe8c4ffe20
Justin LaPolla <j.a.lapolla@gmail.com>
2016-02-28 01:57:01 +0000

    Bump version to 0.1.0
    - Preparing for initial release. Also updated the preamble with
      copyright notice and license information.

--- partials/preamble.js
+++ partials/preamble.js
@@ -1,5 +1,8 @@
 /**
- * LiveBlocks - Declarative model framework for JavaScript
+ * LiveBlocks 0.1.0
  * https://github.com/jlapolla/live-blocks
+ *
+ * Copyright (c) 2016 Justin LaPolla
+ * Released under the MIT license
  */
 
