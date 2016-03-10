dc77d3278a1c1a4aa75902a31dfcad898c914328
Justin LaPolla <j.a.lapolla@gmail.com>
2016-03-10 04:02:05 +0000

    Update publicly exposed classes
    - Turns out we added some new classes that were not exposed no the
      production build yet.

--- partials/footer.js
+++ partials/footer.js
@@ -1,12 +1,18 @@
   }.call(inner, host));
 
   // Expose only public classes and functions on LiveBlocks module
+  var expose = [
+    'BlackBox',
+    'Clock',
+    'ClockedBlock',
+    'ImmediateBlock',
+    'Wire'];
   (function(inner, arr) {
 
     for (var i = 0; i < arr.length; i++) {
 
       this[arr[i]] = inner[arr[i]];
     }
-  }.call(this, inner, ['BlackBox', 'Wire', 'WireConstraint']));
+  }.call(this, inner, expose));
 }.call(this.LiveBlocks, this));
 
