df497d4166ceaf6f28bc8a1b217db436dbb1206f
Justin LaPolla <j.a.lapolla@gmail.com>
2016-03-11 04:08:03 +0000

    Expose new classes
    - Expose new classes on module in production builds.

--- partials/footer.js
+++ partials/footer.js
@@ -1,12 +1,23 @@
   }.call(inner, host));
 
   // Expose only public classes and functions on LiveBlocks module
+  var expose = [
+    'AsyncTimer',
+    'BlackBox',
+    'Clock',
+    'ClockedBlock',
+    'ImmediateBlock',
+    'IntervalTimer',
+    'ManualTimer',
+    'TimedBlock',
+    'Wire',
+  ];
   (function(inner, arr) {
 
     for (var i = 0; i < arr.length; i++) {
 
       this[arr[i]] = inner[arr[i]];
     }
-  }.call(this, inner, ['BlackBox', 'Wire', 'WireConstraint']));
+  }.call(this, inner, expose));
 }.call(this.LiveBlocks, this));
 
